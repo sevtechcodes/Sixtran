e2cc3dcf3e78d6bca01437470284ddbbed083c6e
Sebastian Delgado von Euw <voneuwdel@gmail.com>
2024-06-06 10:12:31 +0200

    create mutations

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,9 +1,3 @@
-// create user
-
-// retrieve user credentials
-
-// pages/api/graphql.js
-
 import { ApolloServer, gql } from 'apollo-server-micro';
 import { PrismaClient } from '@prisma/client';
 
@@ -29,36 +23,79 @@ const typeDefs = gql`
   type Query {
     users: [User]!
 
-    user_by_id(id: ID!): User!
+    userById(id: ID!): User!
 
-    user_by_email(id: ID!): User!
+    userByEmail(email: String!): User!
 
     connectors: [Connector]!
 
-    connectors_by_user(userId: ID!): [Connector]!
+    connectorsByUser(userId: ID!): [Connector]!
   }
 
+  type Mutation {
+    createUser(email: String!, fivetran_api_key: String!, fivetran_api_secret: String!): User!
+
+    createConnector(userId: ID!, name: String!, type: String!): Connector!
+  }
 `;
 
 const resolvers = {
   Query: {
-    user_by_id: async (_, { id }) => {
-      return await prisma.user.findUnique({
-        where: { id: id},
-        include: { connectors: true },
-      });
-    },
-
-    user_by_email: async (_, { email }) => {
-      return await prisma.user.findUnique({
-        where: { email: email},
-        include: { connectors: true },
-      });
-    },
-
-    connectors_by_user: async (_, { userId }) => {
+    users: async () => {
       return await prisma.user.findMany({
-        where: { userId: userId},
+        include: { connectors: true },
+      });
+    },
+
+    userById: async (_, { id }) => {
+      return await prisma.user.findUnique({
+        where: { id },
+        include: { connectors: true },
+      });
+    },
+
+    userByEmail: async (_, { email }) => {
+      return await prisma.user.findUnique({
+        where: { email },
+        include: { connectors: true },
+      });
+    },
+
+    connectors: async () => {
+      return await prisma.connector.findMany({
+        include: { user: true },
+      });
+    },
+
+    connectorsByUser: async (_, { userId }) => {
+      return await prisma.connector.findMany({
+        where: { userId },
+      });
+    },
+  },
+
+  Mutation: {
+    createUser: async (_, { email, fivetran_api_key, fivetran_api_secret }) => {
+      return await prisma.user.create({
+        data: {
+          email,
+          fivetran_api_key,
+          fivetran_api_secret,
+        },
+      });
+    },
+
+    createConnector: async (_, { userId, name, type }) => {
+      return await prisma.connector.create({
+        data: {
+          name,
+          type,
+          user: {
+            connect: {
+              id: userId,
+            }
+          }
+        },
       });
     },
   },
