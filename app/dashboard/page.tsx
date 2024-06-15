ea595bf97c22576433dbfac0c0b90f1fd6dde807
sevtechcodes <sevimtas0101@gmail.com>
2024-06-15 13:05:55 +0200

    Dashboard/page.tsx is done into Typescript

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,200 @@
+'use client';
+
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import { getCookie } from 'cookies-next';
+import { apiCall, modifyConnectors, resyncConnectors, syncConnectors } from '../lib/fivetran';
+import ConnectorTable from '../ui/connector-table';
+import { getConnectors } from '../lib/fivetran';
+
+type Credential = {
+  fivetranApiKey: string;
+  fivetranApiSecret: string;
+};
+
+type Group = {
+  id: string;
+  name: string;
+};
+
+type Connector = {
+  id: string;
+  status: {
+    sync_state: string;
+  };
+  sync_frequency?: number;
+};
+
+type Type = {
+  id: string;
+  name: string;
+};
+
+type ApiResponse<T> = {
+  status: number;
+  body?: {
+    data: {
+      items: T;
+    };
+  };
+};
+
+export default function Page (): React.ReactElement {
+  const [credentials, setCredentials] = useState<Credential | null >(null); //Added null checks to handle cases where cookies might not be present or credentials might be null
+  const [groups, setGroups] = useState<Group[]>([]);
+  const [types, setTypes] = useState<Type[]>([]);
+  const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
+  const [connectors, setConnectors] = useState<Connector[]>([]);
+  const [pending, setPending] = useState<boolean>(true);
+  
+
+  useEffect(() => {
+    const userCookie = getCookie('user');
+
+    if (userCookie && typeof userCookie === 'string') {
+      try {
+        const { fivetranApiKey, fivetranApiSecret } = JSON.parse(userCookie);
+        setCredentials({ fivetranApiKey, fivetranApiSecret });
+
+        const getInitialData = async () => {
+          if (!fivetranApiKey || !fivetranApiSecret) return;
+
+          try {
+            // Get types
+            let response: ApiResponse<Type[]>  | void  = await apiCall('metadata/connector-types?limit=1000', fivetranApiKey, fivetranApiSecret);
+						 if (response && response.body) { //Added to make sure response and the response body are available
+							 const typesData = response.body.data.items;
+							 setTypes(typesData);
+						 }
+
+            // Get groups
+            response = await apiCall('groups', fivetranApiKey, fivetranApiSecret);
+						 if (response && response.body) { //Added to make sure response and the response body are available
+							 const groupsData = response.body.data.items;
+							 setGroups(groupsData);
+							 setSelectedGroup(groupsData[0]);
+						 }
+
+            // Get connectors
+            // Assuming you want to get connectors for the selected group (first group by default)
+						 if (selectedGroup) {
+              const connectorsData = await getConnectors(selectedGroup, fivetranApiKey, fivetranApiSecret);
+              setConnectors(connectorsData);
+              setPending(false);
+            }
+						 
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+
+        getInitialData();
+      } catch (error) {
+        console.error('Error parsing cookie:', error);
+      }
+    } else {
+      console.error('No user cookie found or cookie is not a string');
+    }
+  }, []);
+
+  async function handleSelect (event: ChangeEvent<HTMLSelectElement>) {
+    event.preventDefault();
+    const group= groups.find(g => g.id === event.target.value);
+	
+    if (group && credentials) { //added this line to make sure there are groupt
+      setSelectedGroup(group);
+      setPending(true);
+      const connectorsData = await getConnectors(group, credentials.fivetranApiKey, credentials.fivetranApiSecret);
+      setConnectors(connectorsData);
+      setPending(false);
+    }
+  }
+
+
+  async function pauseConnectors (connectorsToModify: Connector[]) {
+		 if (credentials ) { //checking if credenntials are there
+			 await  modifyConnectors(connectorsToModify, credentials.fivetranApiKey, credentials.fivetranApiSecret, { paused: true });
+			 const updatedConnectors = connectors.map(connector => {
+				 if (connectorsToModify.find(c => c.id === connector.id)) {
+					 return { ...connector, status: { ...connector.status, sync_state: 'paused' } };
+				 }
+				 return connector;
+			 });
+			 setConnectors(updatedConnectors);
+		 } 
+  }
+
+  async function unpauseConnectors (connectorsToModify: Connector[]) {
+		 if (credentials ) { //checking if credenntials are there
+			 await  modifyConnectors(connectorsToModify, credentials.fivetranApiKey, credentials.fivetranApiSecret, { paused: false });
+			 const updatedConnectors = connectors.map(connector => {
+			  if (connectorsToModify.find(c => c.id === connector.id)) {
+					 return { ...connector, status: { ...connector.status, sync_state: 'scheduled' } };
+				 }
+				 return connector;
+			 });
+			 setConnectors(updatedConnectors);
+		 }
+  }
+
+  async function freqConnectors (connectorsToModify: Connector[], freq: number) {
+		 if (credentials ) { //checking if credenntials are there
+			 await  modifyConnectors(connectorsToModify, credentials.fivetranApiKey, credentials.fivetranApiSecret, { sync_frequency: freq });
+			 const updatedConnectors = connectors.map(connector => {
+				 if (connectorsToModify.find(c => c.id === connector.id)) {
+					 return { ...connector, sync_frequency: freq };
+				 }
+				 return connector;
+				 });
+				 setConnectors(updatedConnectors);
+			 }
+	  }
+
+  async function HistResyncConnectors (connectorsToResync: Connector[]) {
+		 if (credentials ) { //checking if credenntials are there
+      await  resyncConnectors(connectorsToResync, credentials.fivetranApiKey, credentials.fivetranApiSecret);
+      const updatedConnectors = connectors.map(connector => {
+        if (connectorsToResync.find(c => c.id === connector.id)) {
+          return { ...connector, status: { ...connector.status, sync_state: 'syncing' } };
+        }
+        return connector;
+      });
+      setConnectors(updatedConnectors);
+	  }
+  }
+
+  async function NormalSyncConnectors (connectorsToSync: Connector[]) {
+			 if (credentials ) { //checking if credenntials are there
+			 await syncConnectors(connectorsToSync, credentials.fivetranApiKey, credentials.fivetranApiSecret);
+			 const updatedConnectors = connectors.map(connector => {
+				 if (connectorsToSync.find(c => c.id === connector.id)) {
+					 return { ...connector, status: { ...connector.status, sync_state: 'syncing' } };
+				 }
+				 return connector;
+			 });
+			 setConnectors(updatedConnectors);
+		 }
+	 }
+
+
+  return (
+    <>
+      <div className='mx-12 mt-5 font-bold text-lg items-center'>
+        <label>Select your connector group: </label>
+        {!pending &&
+        <select onChange={handleSelect} className='bg-[#06AB78] text-white rounded p-1'>
+          {groups.map( (group) => (<option value={group.id} key={group.id}>{group.name}</option>))}
+        </select> }
+      </div>
+      <div className='mx-9'>
+        { !pending && <ConnectorTable data={connectors}
+          types={types}
+          onPause={pauseConnectors}
+          onUnpause={unpauseConnectors}
+          onFreq={freqConnectors}
+          onResync={HistResyncConnectors}
+          onSync={NormalSyncConnectors}
+        /> } 
+      </div>
+    </>
+  );
+}
--- app/ui/credentials-form copy.jsx
+++ app/ui/credentials-form copy.jsx
@@ -1,47 +1,47 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { apiCall } from '../lib/fivetran';
-import { setCookie } from 'cookies-next';
+// import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { apiCall } from '../lib/fivetran';
+// import { setCookie } from 'cookies-next';
 
-export default function CredentialsForm () {
-  const [formData, setFormData] = useState({apiKey: '', apiSecret: ''});
-  const router = useRouter();
+// export default function CredentialsForm () {
+//   const [formData, setFormData] = useState({apiKey: '', apiSecret: ''});
+//   const router = useRouter();
 
-  function handleChange (event) {
-    const {name, value} = event.target;
-    setFormData(prevFormData => ({...prevFormData, [name]: value}));
-  }
+//   function handleChange (event) {
+//     const {name, value} = event.target;
+//     setFormData(prevFormData => ({...prevFormData, [name]: value}));
+//   }
 
-  async function handleSubmit (event) {
-    event.preventDefault();
-    if (formData.apiKey.trim().length === 0 || formData.apiSecret.trim().length === 0) {
-      return;
-    }
+//   async function handleSubmit (event) {
+//     event.preventDefault();
+//     if (formData.apiKey.trim().length === 0 || formData.apiSecret.trim().length === 0) {
+//       return;
+//     }
 
-    const response = await apiCall('account/info', formData.apiKey, formData.apiSecret);
+//     const response = await apiCall('account/info', formData.apiKey, formData.apiSecret);
 
-    if (response.status === 200) {
-      setCookie('user',  JSON.stringify({fivetranApiKey: formData.apiKey, fivetranApiSecret: formData.apiSecret}));
-      router.push('/dashboard');
-    }
+//     if (response.status === 200) {
+//       setCookie('user',  JSON.stringify({fivetranApiKey: formData.apiKey, fivetranApiSecret: formData.apiSecret}));
+//       router.push('/dashboard');
+//     }
 
-  }
+//   }
 
-  return (
-    <div className=''>
-      <form onSubmit={handleSubmit} className='flex flex-col'>
-        <input name='apiKey' type='text' value={formData.apiKey} onChange={handleChange} placeholder='Fill in API Key'
-          className='border mt-10 border-black rounded px-2'
-        ></input>
-        <input name='apiSecret' type='text' value={formData.apiSecret} onChange={handleChange} placeholder='Fill in API Secret'
-          className='border mt-10 border-black rounded px-2'
-        ></input>
-        <button type='submit'
-          className='mt-10 bg-black text-white text-2xl py-1 px-3 rounded-lg font-bold  hover:bg-[#5C5B61]'
-        >Update credentials</button>
-      </form>
-    </ div>
-  );
-}
+//   return (
+//     <div className=''>
+//       <form onSubmit={handleSubmit} className='flex flex-col'>
+//         <input name='apiKey' type='text' value={formData.apiKey} onChange={handleChange} placeholder='Fill in API Key'
+//           className='border mt-10 border-black rounded px-2'
+//         ></input>
+//         <input name='apiSecret' type='text' value={formData.apiSecret} onChange={handleChange} placeholder='Fill in API Secret'
+//           className='border mt-10 border-black rounded px-2'
+//         ></input>
+//         <button type='submit'
+//           className='mt-10 bg-black text-white text-2xl py-1 px-3 rounded-lg font-bold  hover:bg-[#5C5B61]'
+//         >Update credentials</button>
+//       </form>
+//     </ div>
+//   );
+// }
