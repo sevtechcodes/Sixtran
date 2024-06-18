b1d33b28d252cd50cf6e11ed2738fe2c4fa47ba3
Brice <fromm.brice@gmail.com>
2024-06-18 15:21:39 +0200

    mock router working for the test

--- __test__/text.test.tsx
+++ __test__/text.test.tsx
@@ -1,14 +1,19 @@
-import { expect, test, describe, it } from 'vitest';
+import { expect, test, describe, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { mockUseRouter } from '../__mocks__/next/navigation';
 import CredentialsForm from '../app/ui/credentials-form';
 import Hello from '../app/ui/hello';
 
-// test('Home', () => {
-//   render(<CredentialsForm />);
-//   expect(
-//     screen.getByRole('button', { name: 'Update credentials' })
-//   ).toBeDefined();
-// });
+vi.mock('next/navigation', () => ({
+  useRouter: mockUseRouter,
+}));
+
+test('Home', () => {
+  render(<CredentialsForm />);
+  expect(
+    screen.getByRole('button', { name: 'Update credentials' })
+  ).toBeDefined();
+});
 
 describe('Hello component', () => {
   it('renders "Hello World" in an h1 tag', () => {
@@ -16,6 +21,10 @@ describe('Hello component', () => {
     const h1Element = screen.getByText('Hello World');
     expect(h1Element.tagName).toBe('H1');
   });
+});
+
+describe('Test change frequency', () => {
+  it('checks that the user can update the frequency of API refresh', () => {});
 });
 // // jest.mock('next/navigation');
 // beforeAll(async () => {});
