4216fb53915d931299cbfb1c951c6fc426ab5a90
sevtechcodes <sevimtas0101@gmail.com>
2024-06-18 16:28:46 +0200

    Search test

--- __test__/text.test.tsx
+++ __test__/text.test.tsx
@@ -1,7 +1,12 @@
-import { expect, test, describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen} from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import '@testing-library/jest-dom';  // Import jest-dom for the matchers
+import ConnectorDetail from '../app/ui/connector-detail-table';
+import { FiveTranMetaData } from '../app/dashboard/[connector_id]/page'; // Adjust the import path as necessary
 import CredentialsForm from '../app/ui/credentials-form';
 import Hello from '../app/ui/hello';
+// import userEvent from '@testing-library/user-event';
 
 // test('Home', () => {
 //   render(<CredentialsForm />);
@@ -28,3 +33,30 @@ describe('Hello component', () => {
 //     screen.getByRole('button', { name: 'Update credentials' })
 //   ).toBeDefined();
 // });
+
+
+
+describe('ConnectorDetail Component', () => {
+  it('should render search input', () => {
+    const mockSchema: FiveTranMetaData = {
+      name_in_destination: 'Mock Dataset',
+      tables: {},
+      enabled: true,  // Placeholder value
+      schemas: 'schema'  // Placeholder value
+    };
+
+    const { getByPlaceholderText } = render( //The getByPlaceholderText is a query provided by the @testing-library/react library. 
+      <ConnectorDetail
+        schema={mockSchema}
+        queries={[]}
+        disable={() => {}} 
+        enable={() => {}} 
+      />
+    );
+
+    const inputElement = getByPlaceholderText('Search...');
+    expect(inputElement).toBeInTheDocument();
+  });
+});
+
+
